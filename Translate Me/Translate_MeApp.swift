1cf680880d211904fcbe42b99c1963e068566597
fiyinfoluwaafol <fiyinfoluwa.afolayan@gmail.com>
2025-03-24 18:11:24 -0400

    initialized ios project

---
+++ Translate Me/Translate_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Translate_MeApp.swift
+//  Translate Me
+//
+//  Created by Fiyinfoluwa Afolayan on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Translate_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
